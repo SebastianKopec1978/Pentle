e659a1fb6f93647dae6a2bc9966358e96a2d46fb
Your Name <you@example.com>
2022-11-23 19:31:19 +0100

    Initial commit

---
+++ src/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[]args){
+        String[] cats = new String[]{ "Bonifacy", "Garfield", "Klakier", "Pussy"};
+        for (String cat:cats){
+            System.out.println(cat);
+        }
+    }
+}
