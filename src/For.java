05aa428595e4038a3b157771e6fac2c84c2132b6
Your Name <you@example.com>
2022-11-23 20:00:39 +0100

    Pentle For

---
+++ src/For.java
@@ -0,0 +1,10 @@
+public class For {
+    public static void main(String[]args){
+
+        String[] cats = new String[]{ "Tobi", "Daisy","Tofi","Jania"};
+
+        for(int i=0; i<cats.length; i++){
+            System.out.println(cats[i]);
+        }
+    }
+}
