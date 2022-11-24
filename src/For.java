ac9fde57c0b8957df39e4cc818a5961993bf67a2
Your Name <you@example.com>
2022-11-24 14:08:07 +0100

    Switch case

--- src/For.java
+++ src/For.java
@@ -1,10 +1,12 @@
 public class For {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
-        String[] cats = new String[]{ "Tobi", "Daisy","Tofi","Jania"};
+        String[] cats = new String[]{"Tobi", "Daisy", "Tofi", "Jania"};
 
-        for(int i=0; i<cats.length; i++){
+        for (int i = 0; i < cats.length; i++) {
             System.out.println(cats[i]);
+
+
         }
     }
 }
